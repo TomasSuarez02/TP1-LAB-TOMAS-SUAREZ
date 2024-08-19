424079c69829a7adfe8bb66a0c30ba07a1101525
Tomás Suárez <tomassuarez2002@gmail.com>
2024-08-19 11:43:47 -0300

    them.

--- src/Tienda.java
+++ src/Tienda.java
@@ -2,17 +2,4 @@ public class Tienda {
     private String nombreTienda;
     private short cantidadMaxProductosStock;
     private int saldoEnCaja;
-
-    static void newTienda (){
-        Tienda myTienda = new Tienda();
-        myTienda.nombreTienda = "Carrefour";
-        myTienda.cantidadMaxProductosStock = 200;
-        myTienda.saldoEnCaja = 100000;
-    }
-    public static void mostrar (Tienda myTienda){
-        newTienda();
-        System.out.println(myTienda.nombreTienda);
-        System.out.println(myTienda.cantidadMaxProductosStock);
-        System.out.println(myTienda.saldoEnCaja);
-    }
- }
+}
